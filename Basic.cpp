1ff28f8ccc9e0fddf8c8e82dd878ae724f7e92b3
vaibhav <62588796+Vaibhav123v@users.noreply.github.com>
2021-04-09 23:48:10 +0530

    Add files via upload

---
+++ Basic.cpp
@@ -0,0 +1,27 @@
+// C++-programs to find sum of elements in given array
+// array = [1,2,3,4,6,7,8]
+// sum = 1+2+3+4+6+7+8 = 25
+#include<iostream>
+using namespace std;
+int sum_of_elements(int array[],int size)
+{
+    int sum = 0;
+    for(int i = 0; i<size;i++)
+    {
+      sum+=array[i];
+    }
+    return sum;
+}
+int main()
+{
+    int size;
+    cout<<"Enter the size of an array ";
+    cin>>size;
+    int array[size];
+    cout<<"Enter the elements of an array"<<endl;
+    for(int i = 0 ;i<size;i++)
+    {
+        cin>>array[i];
+    }
+    cout<<"Sum of all elements of an array is " <<sum_of_elements(array,size);
+}
